c717fe59280affb147cf0ec1f2059177654b66b2
Riz <80712058+rizznn@users.noreply.github.com>
2021-05-29 23:43:43 -0400

    search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
+var searchFormEl = document.querySelector("#form");
+var cityInputEl = document.querySelector("#searchCity");
+
 var apiKey = '8823044c5dc41939822c0d258eaf31c6';
 var city = 'Toronto';
 
 getData(city);
 function getData(city){
-    fetch(`https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-    .then(function(response) {
-        response.json()
-        .then(function(data) {
+    // format the github api url
+    var apiUrl = `https:api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
             console.log(data);
         });
     });
@@ -14,3 +18,17 @@ function getData(city){
 };
 
 
+var formSubmit = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var searchCity = cityInputEl.value.trim();
+
+    if (searchCity) {
+    getData(searchCity);
+    cityInputEl.value = "";
+    } else {
+    alert("Please enter a city");
+    }
+};
+
+searchFormEl.addEventListener("submit", formSubmit);
